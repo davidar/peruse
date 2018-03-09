cff54f7301ac9321b9afabe70c85451895310a0c
David A Roberts <d@vidr.cc>
2018-03-09 21:17:26 +1000

    standard

--- index.js
+++ index.js
@@ -1,153 +1,163 @@
 #!/usr/bin/env node
 
-var escapeHTML = require("escape-html");
-var jsdom = require("jsdom").jsdom;
-var Readability = require("readability/index").Readability;
-var spawn = require("child_process").spawn;
+let escapeHTML = require('escape-html')
+let jsdom = require('jsdom').jsdom
+let Readability = require('readability/index').Readability
+let spawn = require('child_process').spawn
 
-function removeNode(node) {
-  node.parentNode.removeChild(node);
+function removeNode (node) {
+  node.parentNode.removeChild(node)
 }
 
-function nonempty(node) {
-  return node.textContent.trim() || node.querySelector("img");
+function nonempty (node) {
+  return node.textContent.trim() || node.querySelector('img')
 }
 
-jsdom.env(process.argv[2], [], function(err, window) {
-  if(err) return console.error(err);
-  var document = window.document;
+jsdom.env(process.argv[2], [], function (err, window) {
+  if (err) return console.error(err)
+  let document = window.document
 
-  var links = document.getElementsByTagName("a");
-  for(var i = links.length - 1; i >= 0; i--) {
-    var link = links[i];
-    var href = link.getAttribute("href");
-    if(href && href[0] === "#")
-      link.removeAttribute("href");
+  let links = document.getElementsByTagName('a')
+  for (let i = links.length - 1; i >= 0; i--) {
+    let link = links[i]
+    let href = link.getAttribute('href')
+    if (href && href[0] === '#') {
+      link.removeAttribute('href')
+    }
   }
 
-  var links = document.querySelectorAll("pre a");
-  for(var i = links.length - 1; i >= 0; i--) {
-    var link = links[i];
-    link.outerHTML = link.innerHTML;
+  links = document.querySelectorAll('pre a')
+  for (let i = links.length - 1; i >= 0; i--) {
+    let link = links[i]
+    link.outerHTML = link.innerHTML
   }
 
-  var nodes = document.querySelectorAll("a, em, strong, b, i");
-  for(var i = nodes.length - 1; i >= 0; i--) {
-    var node = nodes[i];
-    if(!nonempty(node))
-      node.outerHTML = node.innerHTML;
+  let nodes = document.querySelectorAll('a, em, strong, b, i')
+  for (let i = nodes.length - 1; i >= 0; i--) {
+    let node = nodes[i]
+    if (!nonempty(node)) {
+      node.outerHTML = node.innerHTML
+    }
   }
 
-  var images = document.getElementsByTagName("img");
-  for(var i = images.length - 1; i >= 0; i--) {
-    var image = images[i];
-    if(image.getAttribute("src") === "")
-      removeNode(image);
+  let images = document.getElementsByTagName('img')
+  for (let i = images.length - 1; i >= 0; i--) {
+    let image = images[i]
+    if (image.getAttribute('src') === '') {
+      removeNode(image)
+    }
   }
 
-  var tables = document.getElementsByTagName("table");
-  for(var i = tables.length - 1; i >= 0; i--) {
-    var table = tables[i];
-    if(table.rows.length === 1 && table.rows[0].cells.length === 1)
-      table.outerHTML = table.rows[0].cells[0].innerHTML;
+  let tables = document.getElementsByTagName('table')
+  for (let i = tables.length - 1; i >= 0; i--) {
+    let table = tables[i]
+    if (table.rows.length === 1 && table.rows[0].cells.length === 1) {
+      table.outerHTML = table.rows[0].cells[0].innerHTML
+    }
   }
 
-  var items = document.getElementsByTagName("li");
-  for(var i = items.length - 1; i >= 0; i--)
-    items[i].removeAttribute("id");
-
-  var codes = document.getElementsByTagName("code");
-  for(var i = codes.length - 1; i >= 0; i--) {
-    var code = codes[i];
-    if(code.textContent.split("\n").length > 2)
-      code.outerHTML = "<pre>" + code.outerHTML + "</pre>";
+  let items = document.getElementsByTagName('li')
+  for (let i = items.length - 1; i >= 0; i--) {
+    items[i].removeAttribute('id')
   }
 
-  var svgs = document.getElementsByTagName("svg");
-  for(var i = svgs.length - 1; i >= 0; i--) {
-    var svg = svgs[i];
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    var dataURI = "data:image/svg+xml," + encodeURIComponent(svg.outerHTML);
-    svg.outerHTML = "<img src=\"" + dataURI + "\" />";
+  let codes = document.getElementsByTagName('code')
+  for (let i = codes.length - 1; i >= 0; i--) {
+    let code = codes[i]
+    if (code.textContent.split('\n').length > 2) {
+      code.outerHTML = '<pre>' + code.outerHTML + '</pre>'
+    }
   }
 
-  var figs = document.getElementsByTagName("figure");
-  for(var i = figs.length - 1; i >= 0; i--) {
-    var fig = figs[i];
-    if(fig.getElementsByTagName("figcaption").length === 0)
-      fig.outerHTML = "<div>" + fig.innerHTML + "</div>";
+  let svgs = document.getElementsByTagName('svg')
+  for (let i = svgs.length - 1; i >= 0; i--) {
+    let svg = svgs[i]
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    let dataURI = 'data:image/svg+xml,' + encodeURIComponent(svg.outerHTML)
+    svg.outerHTML = '<img src="' + dataURI + '" />'
   }
 
-  var breaks = document.querySelectorAll(
-    "h1 br, h2 br, h3 br, h4 br, h5 br, h6 br");
-  for(var i = breaks.length - 1; i >= 0; i--)
-    breaks[i].outerHTML = " ";
+  let figs = document.getElementsByTagName('figure')
+  for (let i = figs.length - 1; i >= 0; i--) {
+    let fig = figs[i]
+    if (fig.getElementsByTagName('figcaption').length === 0) {
+      fig.outerHTML = '<div>' + fig.innerHTML + '</div>'
+    }
+  }
 
-  var waybackToolbar = document.getElementById("wm-ipp");
-  if(waybackToolbar) removeNode(waybackToolbar);
+  let breaks = document.querySelectorAll(
+    'h1 br, h2 br, h3 br, h4 br, h5 br, h6 br')
+  for (let i = breaks.length - 1; i >= 0; i--) {
+    breaks[i].outerHTML = ' '
+  }
 
-  var mwEdits = document.getElementsByClassName("mw-editsection");
-  for(var i = mwEdits.length - 1; i >= 0; i--)
-    removeNode(mwEdits[i]);
+  let waybackToolbar = document.getElementById('wm-ipp')
+  if (waybackToolbar) removeNode(waybackToolbar)
 
-  var span;
-  while((span = document.querySelector("span")))
-    span.outerHTML = span.innerHTML;
+  let mwEdits = document.getElementsByClassName('mw-editsection')
+  for (let i = mwEdits.length - 1; i >= 0; i--) {
+    removeNode(mwEdits[i])
+  }
 
-  var content = document.documentElement.outerHTML;
+  let span
+  while ((span = document.querySelector('span'))) {
+    span.outerHTML = span.innerHTML
+  }
 
-  var loc = document.location;
-  var uri = {
+  let content = document.documentElement.outerHTML
+
+  let loc = document.location
+  let uri = {
     spec: loc.href,
     host: loc.host,
-    prePath: loc.protocol + "//" + loc.host,
-    scheme: loc.protocol.substr(0, loc.protocol.indexOf(":")),
-    pathBase: loc.protocol + "//" + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf("/") + 1),
-  };
-  var readability = new Readability(uri, document);
-  var article = readability.parse();
+    prePath: loc.protocol + '//' + loc.host,
+    scheme: loc.protocol.substr(0, loc.protocol.indexOf(':')),
+    pathBase: loc.protocol + '//' + loc.host + loc.pathname.substr(0, loc.pathname.lastIndexOf('/') + 1)
+  }
+  let readability = new Readability(uri, document)
+  let article = readability.parse()
 
-  if(article) {
-    content = "<h1>" + escapeHTML(article.title) + "</h1>";
+  if (article) {
+    content = '<h1>' + escapeHTML(article.title) + '</h1>'
     content += article.content.replace(
-      /<(embed|iframe|video|audio) /g, "<img ").replace(
-      /<p style="display: inline;" class="readability-styled">([^<]*)<\/p>/g, "$1");
+      /<(embed|iframe|video|audio) /g, '<img ').replace(
+      /<p style="display: inline;" class="readability-styled">([^<]*)<\/p>/g, '$1')
   }
 
-  var markdown = [
-    "markdown",
-    "-bracketed_spans",
-    "-citations",
-    "-escaped_line_breaks",
-    "-fenced_code_attributes",
-    "-fenced_divs",
-    "-header_attributes",
-    "-inline_code_attributes",
-    "-link_attributes",
-    "-multiline_tables",
-    "-native_divs",
-    "-native_spans",
-    "-pipe_tables",
-    "-raw_html",
-    "-simple_tables",
-  ].join("");
+  let markdown = [
+    'markdown',
+    '-bracketed_spans',
+    '-citations',
+    '-escaped_line_breaks',
+    '-fenced_code_attributes',
+    '-fenced_divs',
+    '-header_attributes',
+    '-inline_code_attributes',
+    '-link_attributes',
+    '-multiline_tables',
+    '-native_divs',
+    '-native_spans',
+    '-pipe_tables',
+    '-raw_html',
+    '-simple_tables'
+  ].join('')
 
-  var convert = spawn("pandoc", [
-    "-f", "html",
-    "-t", markdown + "-smart",
-    "--reference-links",
+  let convert = spawn('pandoc', [
+    '-f', 'html',
+    '-t', markdown + '-smart',
+    '--reference-links'
   ], {
-    stdio: ["pipe", "pipe", process.stderr]
-  });
+    stdio: ['pipe', 'pipe', process.stderr]
+  })
 
-  var normalise = spawn("pandoc", [
-    "-f", markdown,
-    "-t", markdown + "-smart",
-    "--reference-links",
+  let normalise = spawn('pandoc', [
+    '-f', markdown,
+    '-t', markdown + '-smart',
+    '--reference-links'
   ].concat(process.argv.slice(3)), {
-    stdio: ["pipe", process.stdout, process.stderr]
-  });
+    stdio: ['pipe', process.stdout, process.stderr]
+  })
 
-  convert.stdin.end(content);
-  convert.stdout.pipe(normalise.stdin);
-});
+  convert.stdin.end(content)
+  convert.stdout.pipe(normalise.stdin)
+})
