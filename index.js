286d8d5228c1fd9d4eb4bd96c3894865e9d275e6
David A Roberts <d@vidr.cc>
2018-04-20 19:25:53 +1000

    syntax highlighting

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
 const bytes = require('utf8-length')
+const chalk = require('chalk')
 const chrono = require('chrono-node')
 const eventToPromise = require('event-to-promise')
 const express = require('express')
 const franc = require('franc')
 const fs = require('fs')
+const Highlights = require('highlights')
 const htmldiff = require('node-htmldiff')
 const iso639 = require('iso-639-3')
 const {JSDOM, VirtualConsole} = require('jsdom')
 const {memoize} = require('lodash')
 const {pandoc} = require('nodejs-sh')
+const parse5 = require('parse5')
 const path = require('path')
 const prerender = require('prerender')
 const querystring = require('querystring')
@@ -36,7 +39,48 @@ const nonempty = node => node.textContent.trim() || node.querySelector('img')
 const removeNode = node => node.parentNode.removeChild(node)
 const removeNodes = nodes => forEachR(nodes, removeNode)
 
+const highlightFormat = {
+  addition: chalk.green,
+  bold: chalk.bold,
+  comment: chalk.gray,
+  deleted: chalk.red,
+  deletion: chalk.red,
+  'front-matter': chalk.bold,
+  heading: chalk.bold,
+  inserted: chalk.green,
+  italic: chalk.italic,
+  'key-value': chalk.gray,
+  link: chalk.blueBright,
+  strike: chalk.strikethrough,
+  substitution: chalk.yellow,
+  tag: chalk.gray,
+  underline: chalk.underline,
+  line: s => s + '\n'
+}
+
+function highlightNode (node) {
+  if (node.type === 'text') return node.data
+  let content = node.childNodes.map(highlightNode).join('')
+  let classes = node.attribs.class ? node.attribs.class.split(' ') : []
+  for (const cls of classes) {
+    if (cls in highlightFormat) content = highlightFormat[cls](content)
+  }
+  return content
+}
+
+function highlight (fileContents, scopeName = 'source.gfm') {
+  let highlighter = new Highlights()
+  highlighter.requireGrammarsSync({
+    modulePath: require.resolve('./language-gfm/package.json')
+  })
+  let html = highlighter.highlightSync({fileContents, scopeName})
+  return parse5.parseFragment(html, {
+    treeAdapter: parse5.treeAdapters.htmlparser2
+  }).childNodes.map(highlightNode).join('')
+}
+
 async function less (output) {
+  if (process.stdout.isTTY) output = highlight(output)
   if (process.stdout.isTTY && output.split(/\n/).length >= process.stdout.rows) {
     let less = spawn('less', [], {
       stdio: ['pipe', process.stdout, process.stderr]
